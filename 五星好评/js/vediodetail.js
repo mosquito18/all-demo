eb419614398f305805f1c398a77c833700985223
linwenjie <530221248@qq.com>
2018-09-28 16:26:27 +0800

    五星评价

--- 五星好评/js/vediodetail.js
+++ 五星好评/js/vediodetail.js
@@ -1,32 +1,35 @@
-function rate(obj, oEvent) {
-    // 图片地址设置
-    var imgSrc = './img/start.png'; //没有填色的星星
-    var imgSrc_2 = './img/start-active.png'; //打分后有颜色的星星,这里的star_full图片时实心的有颜色的五星。
-    if (obj.rateFlag) return;
-    var e = oEvent || window.event;
-    var target = e.target || e.srcElement;
-    var imgArray = obj.getElementsByTagName("img");
-    for (var i = 0; i < imgArray.length; i++) {
-        imgArray[i]._num = i;
-        imgArray[i].onclick = function () {
-            if (obj.rateFlag) return;
-            obj.rateFlag = true;
-            //this._num+1这个值写入到数据库中,作为评分的依据。
-
-            //可以在html页面加一个隐藏域，这里可以通过jquery把这个值赋值给隐藏域，保存的时候传给后台
-        };
-    }
-    if (target.tagName == "IMG") {
-        for (var j = 0; j < imgArray.length; j++) {
-            if (j <= target._num) {
-                imgArray[j].src = imgSrc_2;
-            } else {
-                imgArray[j].src = imgSrc;
-            }
+start_num = 0,
+target_num = 0,
+startArray = (num) => {
+    let imgSrc = "./img/start.png"; //没有填色的星星
+    let imgSrc_2 = "./img/start-active.png"; //打分后有颜色的星星,这里的star_full图片时实心的有颜色的五星。
+    let imgArray = $(".start-list img");
+    for (let j = 0; j < imgArray.length; j++) {
+        if (j <= num - 1) {
+            imgArray.eq(j).attr("src", imgSrc_2);
+        } else {
+            imgArray.eq(j).attr("src", imgSrc);
         }
+    }
+};
+$(".start-list img").mouseover(function () {
+    target_num = this.dataset.index;
+    if (target_num < start_num) {
+        startArray(start_num);
     } else {
-        for (var k = 0; k < imgArray.length; k++) {
-            imgArray[k].src = imgSrc;
-        }
+        startArray(target_num);
     }
-}
+
+});
+
+$(".start-list img").mouseout(function () {
+    if (start_num > 0) {
+        startArray(start_num);
+    } else {
+        startArray(0);
+    }
+});
+$(".start-list img").click(function () {
+    start_num = this.dataset.index;
+    startArray(start_num);
+});
