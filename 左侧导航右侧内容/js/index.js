eee089d8e6197174cb5f5bde088ee10a707f2270
linwenjie <530221248@qq.com>
2018-09-28 16:45:23 +0800

    左侧导航栏

--- 左侧导航右侧内容/js/index.js
+++ 左侧导航右侧内容/js/index.js
@@ -1,17 +1,17 @@
 $(function () {
-	$('#lanPos').css('top', $('.hover').offset().top);
-	$('ul li').hover(function () {
-		$('#lanPos').css('top', $(this).offset().top);
+    $('#lanPos').css('top', $('.left-nav .hover').offset().top-$('.left-nav').offset().top);
+	$('.left-nav ul li').hover(function () {
+		$('#lanPos').css('top', $(this).offset().top-$('.left-nav').offset().top);
 	}, function () {
-		$('#lanPos').css('top', $('.hover').offset().top);
+		$('#lanPos').css('top', $('.left-nav .hover').offset().top-$('.left-nav').offset().top);
 	})
 
-	$('ul li').click(function () {
-		for (var i = 0; i < $('ul li').size(); i++) {
-			if (this == $('ul li').get(i)) {
-				$('ul li').eq(i).children('a').addClass('hover');
+	$('.left-nav ul li').click(function () {
+		for (var i = 0; i < $('.left-nav ul li').size(); i++) {
+			if (this == $('.left-nav ul li').get(i)) {
+				$('.left-nav ul li').eq(i).children('a').addClass('hover');
 			} else {
-				$('ul li').eq(i).children('a').removeClass('hover');
+				$('.left-nav ul li').eq(i).children('a').removeClass('hover');
 			}
 		}
 	})
